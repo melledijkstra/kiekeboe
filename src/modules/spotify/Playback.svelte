b2b49cc5f3e00617a2c4d8d67d9d0c7c1b17967e
Melle Dijkstra <melle.dijkstra@adidas.com>
2025-01-10 09:47:40 +0100

    added playback feedback in the spotify player UI

--- src/modules/spotify/Playback.svelte
+++ src/modules/spotify/Playback.svelte
@@ -4,6 +4,9 @@
   import { initializeSpotifyPlayer, transferPlaybackDevice } from "./player"
   import Icon from "@/components/Icon.svelte"
   import { mdiPause, mdiPlay, mdiReload, mdiShuffleVariant, mdiSkipNext, mdiSkipPrevious } from "@mdi/js"
+  import { millisecondsToTime } from "./utils"
+
+  let EMPTY: undefined;
 
   const { token } = $props()
   let deviceId = $state<string | null>(null)
@@ -11,6 +14,14 @@
   let isActive = $state(false)
   let isPaused = $state(true)
   let currentTrack = $state<Spotify.Track | undefined>(undefined)
+  let songDuration = $state(0) // in ms
+  let position = $state(0) // in ms
+  let remaining = $derived(songDuration - position)
+  let completedPercent = $derived(Math.ceil((position / songDuration) * 100))
+  let timeLeft = $derived<string>(millisecondsToTime(remaining))
+  let currentTime = $derived<string>(millisecondsToTime(position))
+  
+  let playbackLoop = $state<number>()
 
   async function playerStateChanged(state: Spotify.PlaybackState) {
     log('state change', state)
@@ -23,6 +34,20 @@
 
     isPaused = state.paused
     currentTrack = state.track_window.current_track
+    songDuration = state.track_window.current_track.duration_ms
+    position = state.position
+
+    if (!state.paused) {
+      if (!playbackLoop) {
+        playbackLoop = setInterval(() => {
+          log('updating time')
+          position += 1000
+        }, 1000)
+      }
+    } else {
+      clearInterval(playbackLoop)
+      playbackLoop = EMPTY
+    }
 
     isActive = !!(await player?.getCurrentState())
   }
@@ -64,46 +89,49 @@
     if (player) {
       player.disconnect()
     }
+    if (playbackLoop) {
+      clearInterval(playbackLoop)
+      playbackLoop = EMPTY
+    }
   })
 </script>
 
 <div class="min-w-80 w-fit max-w-96 overflow-hidden rounded-xl border-b-2 border-t-2 border-white/20 bg-neutral-400/50 p-5 text-white shadow-md">
-  {#if isActive}
-    {#if currentTrack}
-    <div class="flex flex-row gap-4">
-      <img class="size-20 rounded" src={currentTrack.album.images[0].url} alt={currentTrack.name} />
-      <div class="flex flex-col justify-center overflow-hidden">
-        <strong class="truncate text-xl">{currentTrack.name}</strong>
-        <p class="truncate">{currentTrack.artists.map(artist => artist.name).join(', ')}</p>
-      </div>
+  {#if currentTrack}
+  <div class="flex flex-row gap-4">
+    <img class="size-20 rounded" src={currentTrack.album.images[0].url} alt={currentTrack.name} />
+    <div class="flex flex-col justify-center overflow-hidden">
+      <strong class="truncate text-xl">{currentTrack.name}</strong>
+      <p class="truncate">{currentTrack.artists.map(artist => artist.name).join(', ')}</p>
     </div>
-    <div class="mt-2 flex w-full flex-row justify-between text-sm">
-      <div>0:11</div>
-      <div>-2:14</div>
-    </div>
-    <hr style="width: 40%;" class="mb-4 h-1 rounded-full bg-white outline-none" />
-    <div class="flex flex-row justify-between">
-      <button disabled>
-        <Icon class="size-6 fill-white" path={mdiShuffleVariant} />
-      </button>
-      <button onclick={() => player?.previousTrack()}>
-        <Icon class="size-6 fill-white" path={mdiSkipPrevious} />
-      </button>
-      <button onclick={() => player?.togglePlay()}>
-        <Icon class="size-6 fill-white" path={isPaused ? mdiPlay : mdiPause} />
-      </button>
-      <button onclick={() => player?.nextTrack()}>
-        <Icon class="size-6 fill-white" path={mdiSkipNext} />
-      </button>
-      <button disabled>
-        <Icon class="size-6 fill-white" path={mdiReload} />
-      </button>
-    </div>
-    {:else}
-      <p>No active track, select one first</p>
-    {/if}
+  </div>
+  <div class="mt-2 flex w-full flex-row justify-between text-sm">
+    <div>{currentTime}</div>
+    <div>-{timeLeft}</div>
+  </div>
+  <hr style="width: {completedPercent}%;" class="mb-4 h-1 rounded-full bg-white outline-none" />
+  <div class="flex flex-row justify-between">
+    <button disabled>
+      <Icon class="size-6 fill-white" path={mdiShuffleVariant} />
+    </button>
+    <button onclick={() => player?.previousTrack()}>
+      <Icon class="size-6 fill-white" path={mdiSkipPrevious} />
+    </button>
+    <button onclick={() => player?.togglePlay()}>
+      <Icon class="size-6 fill-white" path={isPaused ? mdiPlay : mdiPause} />
+    </button>
+    <button onclick={() => player?.nextTrack()}>
+      <Icon class="size-6 fill-white" path={mdiSkipNext} />
+    </button>
+    <button disabled>
+      <Icon class="size-6 fill-white" path={mdiReload} />
+    </button>
+  </div>
   {:else}
-    <p>This is currently not the active device, transfer playback to this device</p>
+    <p>No active track, select one first</p>
+  {/if}
+  {#if !isActive}
+    <p class="mt-2">This is currently not the active device, transfer playback to this device</p>
     <button onclick={forceActivateDevice} class="my-2 p-2 bg-zinc-900 hover:bg-zinc-600 rounded">Force activate this device</button>
   {/if}
   
