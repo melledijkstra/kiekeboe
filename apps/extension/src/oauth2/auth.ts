3be95f99220f7b0889d4de3a2e7081f3ec4ad514
Melle Dijkstra <melle.dijkstra@adidas.com>
2025-07-06 15:53:49 +0200

    fix: correct environment variable references

--- apps/extension/src/oauth2/auth.ts
+++ apps/extension/src/oauth2/auth.ts
@@ -50,14 +50,6 @@ export class AuthClient {
 
   async isAuthenticated(): Promise<boolean> {
     try {
-      if (this.provider.name === 'google' && typeof chrome !== 'undefined') {
-        const token = await this.getAuthTokenChrome()
-        if (token) {
-          return true
-        }
-        return false
-      }
-  
       const token = await this.getAuthToken()
   
       this.logger.log({ token })
@@ -73,12 +65,8 @@ export class AuthClient {
   }
 
   async getAuthTokenChrome(interactive = false): Promise<string | undefined> {
-    try {
-      const oauth2 = await chrome.identity.getAuthToken({ interactive })
-      return oauth2?.token
-    } catch (error) {
-      this.logger.error(error)
-    }
+    const oauth2 = await chrome.identity.getAuthToken({ interactive })
+    return oauth2?.token
   }
 
   async deauthenticateChrome(): Promise<boolean> {
@@ -223,14 +211,17 @@ export class AuthClient {
       // if we are on chrome browser then use the preferred auth token
       // method that is already build in
       try {
+        this.logger.log(
+          'trying to retrieve oauth token using build in functionality'
+        )
         const token = await this.getAuthTokenChrome(interactive)
         if (token) {
           return token
         }
-      } catch (e) {
-        console.error(
+      } catch (error) {
+        this.logger.warn(
           `${this.provider.name}: No luck retrieving oauth token using build in functionality, trying manually`,
-          e
+          error
         )
       }
     }
